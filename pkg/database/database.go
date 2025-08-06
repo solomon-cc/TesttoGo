8265759557e49f78f16d65234a7a5647d7368c0b
solomon <cheng.chen348434@seres.cn>
2025-08-06 17:05:50 +0800

    fix listpapter

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -2,18 +2,20 @@ package database
 
 import (
 	"fmt"
+	"time"
 
 	"testogo/internal/model/entity"
 	"testogo/pkg/config"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
 )
 
 var DB *gorm.DB
 
 func Init() error {
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=%s&parseTime=True&loc=Local",
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=%s&parseTime=True&loc=Local&timeout=10s&writeTimeout=30s&readTimeout=30s",
 		config.GetString("database.username"),
 		config.GetString("database.password"),
 		config.GetString("database.host"),
@@ -22,7 +24,15 @@ func Init() error {
 		config.GetString("database.charset"),
 	)
 
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(mysql.New(mysql.Config{
+		DSN:               dsn,
+		DefaultStringSize: 191, // MySQL 5.7 的 utf8mb4 索引长度限制
+	}), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{
+			SingularTable: true, // 使用单数表名
+		},
+		DisableForeignKeyConstraintWhenMigrating: true, // 禁用外键约束
+	})
 	if err != nil {
 		return err
 	}
@@ -35,6 +45,7 @@ func Init() error {
 
 	sqlDB.SetMaxIdleConns(config.GetInt("database.maxIdleConns"))
 	sqlDB.SetMaxOpenConns(config.GetInt("database.maxOpenConns"))
+	sqlDB.SetConnMaxLifetime(time.Hour) // 设置连接最大生命周期
 
 	// 自动迁移数据库表
 	err = db.AutoMigrate(
