9ff663436f5a48dbea1761981dee321b3fe44de8
solomon <cheng.chen348434@seres.cn>
2025-09-12 18:04:11 +0800

    update cors rule

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"net/http"
+	"regexp"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,19 +12,15 @@ func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		origin := c.Request.Header.Get("Origin")
 
-		// 允许localhost的所有端口
-		if origin == "http://localhost:3000" ||
-			origin == "http://localhost:8080" ||
-			origin == "http://localhost:3001" ||
-			origin == "http://127.0.0.1:3000" ||
-			origin == "http://127.0.0.1:8080" ||
-			origin == "http://127.0.0.1:5173" {
-			c.Header("Access-Control-Allow-Origin", origin)
-		}
+		// 允许 localhost 任意端口
+		matchLocalhost, _ := regexp.MatchString(`^http://(localhost|127\.0\.0\.1)(:\d+)?$`, origin)
 
-		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		c.Header("Access-Control-Allow-Headers", "Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-		c.Header("Access-Control-Allow-Credentials", "true")
+		if matchLocalhost || origin == "https://go.ylmz.com.cn" {
+			c.Header("Access-Control-Allow-Origin", origin)
+			c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+			c.Header("Access-Control-Allow-Headers", "Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+			c.Header("Access-Control-Allow-Credentials", "true")
+		}
 
 		// 处理预检请求
 		if c.Request.Method == "OPTIONS" {
