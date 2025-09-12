9761526382353679c9961f093ec325a7085f5067
solomon <843294283@qq.com>
2025-09-13 07:25:16 +0800

    fix cors

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -12,8 +12,21 @@ func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		origin := c.Request.Header.Get("Origin")
 
+<<<<<<< HEAD
 		// 允许 localhost 任意端口
 		matchLocalhost, _ := regexp.MatchString(`^http://(localhost|127\.0\.0\.1)(:\d+)?$`, origin)
+=======
+		// 允许localhost的所有端口
+		if origin == "http://localhost:3000" ||
+			origin == "http://localhost:8080" ||
+			origin == "http://localhost:3001" ||
+			origin == "https://go.ylmz.com.cn" ||
+			origin == "http://127.0.0.1:3000" ||
+			origin == "http://127.0.0.1:8080" ||
+			origin == "http://127.0.0.1:5173" {
+			c.Header("Access-Control-Allow-Origin", origin)
+		}
+>>>>>>> 9e0c830 (fix cors)
 
 		if matchLocalhost || origin == "https://go.ylmz.com.cn" {
 			c.Header("Access-Control-Allow-Origin", origin)
